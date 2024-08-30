142a542ebab65bc02afb2654b97892cf007afb3e
Himanshu Joshi <himanshu1660.be22@chitkara.edu.in>
2024-08-30 11:33:01 +0530

    all changes are configured

--- 01_backend/src/app.js
+++ 01_backend/src/app.js
@@ -12,11 +12,20 @@ app.use(cors({
     credentials:true
 }))
 // accepting json file 
-app.use(expreess.json({limit:"16kb"}))
+app.use(express.json({limit:"16kb"}))
 // data coming from url  
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 // to store favicon asset we use static
 app.use(express.static("public")) 
 app.use(cookieParser)
 
-export{ app }
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+// https://localhost:8000/api/v1/users/register
+
+export{ app } 
